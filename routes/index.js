4c3f1a111d20bb189fefdda9472358c7623af07e
Shweta Bhagat <bhagatshweta0216@gmail.com>
2023-11-19 22:48:59 +0530

    add image gallery

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const session = require('express-session')
 const isLoggedIn = require('../middleware/isLoggedIn')
-
+const multer = require('multer')
+const Image = require('../model/Image')
 
 // render the login page with functionality to login with google
 router.get('/', (req, res) => {
@@ -22,9 +23,28 @@ router.get('/logout', (req, res) => {
     })
 })
 
+// Multer configuration for file uploads
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post('/image/upload', upload.single('image'), (req, res) => {
+    const newImage = new Image();
+    newImage.name = req.file.originalname;
+    newImage.data = req.file.buffer;
+    newImage.contentType = req.file.mimetype;
+
+    newImage.save();
+    res.redirect('/image/gallery')
+});
+
+router.get('/image/gallery', async (req, res) => {
+    const images = await Image.find()
+    res.render('images', { images });
+    
+})
 
 // successfull redirect to editor route 
-router.get('/editor', isLoggedIn, (req, res) => {
+router.get('/image/editor', isLoggedIn, (req, res) => {
     res.render('editor')
 })
 
